28bccdcb83e42420f29152a1b3cfe74356a0e0b3
Jackson Ming Hu <huming2207@gmail.com>
2018-03-09 10:46:10 +1100

    GUI: implement main GUI & controllers

--- src/main/java/EnlighterMain.java
+++ src/main/java/EnlighterMain.java
@@ -1,16 +1,33 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class EnlighterMain extends Application
 {
+    private Stage mainStage;
+
     public static void main(String[] args)
     {
-
+        Application.launch(EnlighterMain.class, args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception
     {
+        this.mainStage = primaryStage;
+        this.mainStage.setTitle("Enlighter - Enlight Configuration Tool");
 
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(EnlighterMain.class.getResource("fxml/Home.fxml"));
+
+            mainStage.setScene(new Scene(loader.load()));
+            mainStage.show();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
