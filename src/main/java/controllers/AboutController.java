d7e33ed27da36dd3086986486b4daecff5c3b868
Jackson Ming Hu <huming2207@gmail.com>
2018-03-12 17:30:09 +1100

    GUI: About: implement controller

--- src/main/java/controllers/AboutController.java
+++ src/main/java/controllers/AboutController.java
@@ -1,5 +1,18 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class AboutController
 {
+    @FXML
+    private Button okayButton;
+
+    public void handleOkayButton()
+    {
+        Stage stage = (Stage)okayButton.getScene().getWindow();
+        stage.close();
+    }
 }
