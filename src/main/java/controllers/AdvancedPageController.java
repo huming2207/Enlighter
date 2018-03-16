13a0acb8559bf21c5e71b12215c1cd5da80d2fa9
Jackson Ming Hu <huming2207@gmail.com>
2018-03-16 21:29:42 +1100

    Gui: Advanced: implement WiFi settings

--- src/main/java/controllers/AdvancedPageController.java
+++ src/main/java/controllers/AdvancedPageController.java
@@ -1,21 +1,26 @@
 package controllers;
 
+import helpers.EnlightSettingPusher;
 import helpers.EnlightSysInfoFetcher;
 import helpers.interfaces.EnlightInfoFetcher;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
+import javafx.scene.control.TextField;
 import models.SysInfo;
+
+import javax.xml.soap.Text;
+import java.util.HashMap;
 
 public class AdvancedPageController
 {
     private SysInfo sysInfo = new SysInfo();
     private EnlightInfoFetcher infoFetcher = new EnlightSysInfoFetcher(this.sysInfo);
+    private EnlightSettingPusher settingPusher;
 
     public AdvancedPageController(String deviceIp)
     {
+        this.settingPusher = new EnlightSettingPusher(deviceIp);
         infoFetcher.fetchInfo(deviceIp);
     }
 
@@ -44,6 +49,16 @@ public class AdvancedPageController
     private Label wifiSigStrength;
 
     @FXML
+    private TextField firmwarePathTextBox;
+
+    @FXML
+    private TextField wifiSsidTextBox;
+
+    @FXML
+    private TextField wifiPasswdTextBox;
+
+
+    @FXML
     private void initialize()
     {
         // Binding starts here...
@@ -55,5 +70,41 @@ public class AdvancedPageController
         currentWifiSsid.textProperty().bind(this.sysInfo.netSsidProperty());
         currentIpAddr.textProperty().bind(this.sysInfo.netIpProperty());
         wifiSigStrength.textProperty().bind(this.sysInfo.netSigProperty().asString());
+    }
+
+    @FXML
+    private void handleWifiSave()
+    {
+        HashMap<String, String> map = new HashMap<>();
+
+        if(wifiSsidTextBox.getText().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "WiFi name cannot be empty!");
+            alert.showAndWait();
+            return; // Stops here as this is an error
+        } else {
+            map.put("wifi_ssid", wifiSsidTextBox.getText());
+        }
+
+        if(!wifiPasswdTextBox.getText().isEmpty()) {
+            map.put("wifi_passwd", wifiPasswdTextBox.getText());
+        }
+
+        settingPusher.commitSetting(map, "setting");
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
+                "WiFi settings has been updated, please wait until your device reboot.");
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void handleFirmwareSelection()
+    {
+
+    }
+
+    @FXML
+    private void handleFirmwareUpgrade()
+    {
+
     }
 }
